ec9bbefe58370bdfc75ab8ee40020956dfaf1b54
Anton Ponevejski <ponevejski@gmail.com>
2022-10-24 11:27:45 +0300

    change some style logic - refactored cards template to grid instead flex

--- src/components/searchField/SearchField.js
+++ src/components/searchField/SearchField.js
@@ -1,44 +1,36 @@
-import React from 'react';
+import React from "react";
 
-import { TextField, InputAdornment } from '@mui/material';
-import SearchIcon from '@mui/icons-material/Search';
-import lodash from 'lodash';
+import { TextField } from "@mui/material";
+import lodash from "lodash";
 
-import './style.css';
-import { UNITS } from '../../data';
-
-
+import "./style.css";
+import { UNITS } from "../../data";
 
 const SearchField = ({ setFilteredCards }) => {
-	const filterCards = lodash.debounce((event) => {
-		const filteredValues = UNITS.filter((card) =>
-			card.name.includes(event.target.value)
-		);
+  const filterCards = lodash.debounce((event) => {
+    const filteredValues = UNITS.filter((card) =>
+      card.name.includes(event.target.value)
+    );
 
-		setFilteredCards(filteredValues);
-	}, 800);
+    setFilteredCards(filteredValues);
+  }, 800);
 
-	return (
-		<div className='search-wrapper'>
-			<TextField
-				id='full-width-text-field'
-				placeholder='Search deliveries by units'
-				variant='standard'
-				style={{ width: '50%' }}
-				InputProps={{
-					startAdornment: (
-						<InputAdornment position='start'>
-							<SearchIcon />
-						</InputAdornment>
-					),
-					inputProps: {
-						style: { textAlign: 'center', fontSize: '1.5rem' },
-					},
-				}}
-				onChange={filterCards}
-			/>
-		</div>
-	);
+  return (
+    <div className="search-wrapper">
+      <TextField
+        id="full-width-text-field"
+        placeholder="🔍 Search deliveries by units"
+        variant="standard"
+        style={{ width: "50%" }}
+        InputProps={{
+          inputProps: {
+            style: { textAlign: "center", fontSize: "1.5rem" },
+          },
+        }}
+        onChange={filterCards}
+      />
+    </div>
+  );
 };
 
 export default SearchField;
